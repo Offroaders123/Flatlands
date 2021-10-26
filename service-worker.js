0fb37b238c97b4d5af74a322dbb02f0291ee162c
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2021-10-26 13:47:45 -0700

    On the Way to PWA!
    - Added various PWA features! Now the site is almost installable on Chromium browsers too. More work has to be done with the Service Worker, as a few errors seem to be occurring with image fetching.
    - Organized the locations of textures for the game, now they are located in Resources or Textures depending on whether the texture is for inside the game itself, or for app / web functionality.
    
    Still more to fix!
    - Image fetching is happening too often, the sprites should only be loaded one time. I will come up with a caching system that saves the sprites on page load, then any references can go to those instead.
    - The app's Service Worker hasn't been enabled yet, it's fetch event listener is caught up in the problem of the sprite loading.

---
+++ service-worker.js
@@ -0,0 +1,17 @@
+self.Flatlands = {
+  version: 0.34
+}
+self.addEventListener("activate",event => {
+  event.waitUntil(caches.keys().then(versions => Promise.all(versions.map(cache => {
+    if (cache !== Flatlands.version) return caches.delete(cache);
+  }))));
+  event.waitUntil(clients.claim());
+});
+self.addEventListener("fetch",event => {
+  event.respondWith(caches.match(event.request).then(response => {
+    return response || fetch(event.request).then(async response => {
+      caches.open(Flatlands.version).then(cache => cache.put(event.request,response));
+      return response.clone();
+    });
+  }));
+});
