a0183450b53979780c34ca2f307cd89485228d5c
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2023-07-25 14:48:20 -0700

    Feature Typings
    I'm going to work on re-integrating the feature definitions back into the game, as right now they are un-typed, and it's making it hard to tell where parts of the codebase are being defined. I will find a new, better way to implement add-ons, maybe using JS instead of plain JSON objects. I like that MC does that, but since Flatlands is written in JS anyways, might as well just provide the internal workings of the game's implementation to the 'Add-On API' so you can actually just mod the game using ESM!

--- src/properties.ts
+++ src/properties.ts
@@ -1,5 +1,89 @@
 import { ctx } from "./canvas.js";
 
+export interface BaseDefinition {
+  name: string;
+  texture: {
+    source: string;
+    readonly image: HTMLElement;
+    directional?: false;
+  };
+}
+
+export interface AnimatedDefinition extends BaseDefinition {
+  animation: Animation;
+}
+
+export type Animation = ReactiveAnimation | RepeatAnimation;
+
+export interface ReactiveAnimation {
+  type: "reactive";
+  duration: number;
+  keyframes: number;
+  columns: number;
+}
+
+export interface RepeatAnimation {
+  type: "repeat";
+  duration: number;
+  keyframes: number;
+}
+
+export interface Player extends AnimatedDefinition {
+  box: {
+    width: 16;
+    height: 32;
+  };
+  animation: ReactiveAnimation;
+  direction: {
+    horizontal: string;
+    vertical: false | "down" | "up";
+  };
+  hotbar: {
+    slots: [string,string,string,string,string,string];
+    active: number;
+  };
+  speed: number;
+}
+
+export interface Fire extends AnimatedDefinition {
+  texture: BaseDefinition["texture"] & {
+    directional: false;
+  };
+  animation: RepeatAnimation;
+}
+
+export interface Ground extends BaseDefinition {
+  texture: BaseDefinition["texture"] & {
+    pattern: CanvasPattern;
+  };
+}
+
+export interface Tree extends BaseDefinition {
+  box: {
+    width: number;
+    height: number;
+  };
+}
+
+export interface Definitions {
+  entity: {
+    player: Player;
+    shadow: BaseDefinition;
+  };
+  item: {
+    fire: Fire;
+    hatchet: BaseDefinition;
+    pickmatic: BaseDefinition;
+    pizza: BaseDefinition;
+    spade: BaseDefinition;
+    spearsword: BaseDefinition;
+  };
+  terrain: {
+    ground: Ground;
+    tree: Tree;
+  };
+};
+
 const missingTextureSprite = new Image();
 missingTextureSprite.src = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAIAAAACAQMAAABIeJ9nAAAAAXNSR0IB2cksfwAAAAlwSFlzAAALEwAACxMBAJqcGAAAAAZQTFRF+QD/AAAASf/37wAAAAxJREFUeJxjcGBoAAABRADBOnocVgAAAABJRU5ErkJggg==";
 
