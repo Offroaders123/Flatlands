7d4bd4b790bc8d7eaa1b3fa6164e5980d8a27ec3
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2023-07-30 01:18:28 -0700

    Feature Typings - Stable
    Meant to commit this yesterday. Have a few updates to the tsconfig that I was going to include here too, which held back not finishing this before just pushing it to production. Realized when looking at it again, those should probably come as separate changes apart from the ones of pushing the feature typings to production.

--- public/service-worker.js
+++ public/service-worker.js
@@ -4,7 +4,7 @@
 var self = /** @type { ServiceWorkerGlobalScope } */ (/** @type { unknown } */ (globalThis));
 
 const Flatlands = {
-  version: "Flatlands v0.10.0",
+  version: "Flatlands v0.14.0",
   cache: true
 };
 
